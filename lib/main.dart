ab2d1fe29b30d693412d937582437b909bf648ad
Waleed <waleed_arshad006@hotmail.com>
2018-09-30 02:14:17 +0500

    Add bar progress indicator

--- lib/main.dart
+++ lib/main.dart
@@ -9,32 +9,15 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'Flutter Demo',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(title: 'Progress indicators'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -43,67 +26,169 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return new Scaffold(
       appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: new Text(widget.title),
       ),
       body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             new Text(
-              'You have pushed the button this many times:',
+              'Cellular bar progress indicator',
+              style: Theme.of(context).textTheme.title,
             ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            ProgressIndicators(
+              numberOfBars: 4,
+              color: Colors.grey,
+              fontSize: 10.0,
+              barSpacing: 2.0,
+              beginTweenValue: 5.0,
+              endTweenValue: 10.0,
+              milliseconds: 200,
             ),
           ],
         ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class ProgressIndicators extends StatefulWidget {
+  final int numberOfBars;
+  final double fontSize;
+  final double barSpacing;
+  final Color color;
+  final int milliseconds;
+  final double beginTweenValue;
+  final double endTweenValue;
+
+  ProgressIndicators({
+    this.numberOfBars = 3,
+    this.fontSize = 10.0,
+    this.color = Colors.black,
+    this.barSpacing = 0.0,
+    this.milliseconds = 250,
+    this.beginTweenValue = 5.0,
+    this.endTweenValue = 10.0,
+  });
+
+  _ProgressIndicatorsState createState() => _ProgressIndicatorsState(
+        numberOfBars: this.numberOfBars,
+        fontSize: this.fontSize,
+        color: this.color,
+        barSpacing: this.barSpacing,
+        milliseconds: this.milliseconds,
+        beginTweenValue: this.beginTweenValue,
+        endTweenValue: this.endTweenValue,
+      );
+}
+
+class _ProgressIndicatorsState extends State<ProgressIndicators>
+    with TickerProviderStateMixin {
+  int numberOfBars;
+  int milliseconds;
+  double fontSize;
+  double barSpacing;
+  Color color;
+  double beginTweenValue;
+  double endTweenValue;
+  List<AnimationController> controllers = new List<AnimationController>();
+  List<Animation<double>> animations = new List<Animation<double>>();
+  List<Widget> _widgets = new List<Widget>();
+
+  _ProgressIndicatorsState({
+    this.numberOfBars,
+    this.fontSize,
+    this.color,
+    this.barSpacing,
+    this.milliseconds,
+    this.beginTweenValue,
+    this.endTweenValue,
+  });
+
+  initState() {
+    super.initState();
+    for (int i = 0; i < numberOfBars; i++) {
+      _addAnimationControllers();
+      _buildAnimations(i);
+      _addListOfDots(i);
+    }
+
+    controllers[0].forward();
+  }
+
+  void _addAnimationControllers() {
+    controllers.add(AnimationController(
+        duration: Duration(milliseconds: milliseconds), vsync: this));
+  }
+
+  void _addListOfDots(int index) {
+    _widgets.add(Padding(
+      padding: EdgeInsets.only(right: barSpacing),
+      child: _AnimatingBar(
+        animation: animations[index],
+        fontSize: fontSize,
+        color: color,
+      ),
+    ));
+  }
+
+  void _buildAnimations(int index) {
+    animations.add(
+        Tween(begin: widget.beginTweenValue, end: widget.endTweenValue)
+            .animate(controllers[index])
+              ..addStatusListener((AnimationStatus status) {
+                if (status == AnimationStatus.completed)
+                  controllers[index].reverse();
+                if (index == numberOfBars - 1 &&
+                    status == AnimationStatus.dismissed) {
+                  controllers[0].forward();
+                }
+                if (animations[index].value > widget.endTweenValue / 2 &&
+                    index < numberOfBars - 1) {
+                  controllers[index + 1].forward();
+                }
+              }));
+  }
+
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 30.0,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: _widgets,
+      ),
+    );
+  }
+
+  dispose() {
+    for (int i = 0; i < numberOfBars; i++) controllers[i].dispose();
+    super.dispose();
+  }
+}
+
+class _AnimatingBar extends AnimatedWidget {
+  final Color color;
+  final double fontSize;
+  _AnimatingBar(
+      {Key key, Animation<double> animation, this.color, this.fontSize})
+      : super(key: key, listenable: animation);
+
+  Widget build(BuildContext context) {
+    final Animation<double> animation = listenable;
+    return Container(
+      height: animation.value,
+      decoration: BoxDecoration(
+        shape: BoxShape.rectangle,
+        border: Border.all(color: color),
+        borderRadius: BorderRadius.circular(2.0),
+        color: color,
+      ),
+      width: 5.0,
     );
   }
 }
